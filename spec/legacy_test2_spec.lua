a5a9b9ad6bcb3f156250e704b362a7b88c9c3f20
recih <fengbocih+github@gmail.com>
2018-05-31 19:47:45 +0800

    use busted for unit test

---
+++ spec/legacy_test2_spec.lua
@@ -0,0 +1,72 @@
+local tracedoc = require("tracedoc")
+
+describe("original test2.lua from cloudwu/tracedoc", function()
+    local print = print
+
+    setup(function()
+        print = function(...) end
+    end)
+
+    test("test2.lua", function()
+        local doc = tracedoc.new {}
+
+        doc.a = false
+        doc.b = { x = 1, y = 2 }
+        doc.c = { a = 3, b = 4 }
+
+        tracedoc.opaque(doc.c, true)
+
+        local function map(str)
+            return function (doc, v)
+                print(str, v)
+            end
+        end
+
+        local function add_b(doc, x, y)
+            print("b.x+b.y=", x+y)
+        end
+
+        local function add_c(doc, c)
+            print("c.a+c.b=", c.a + c.b)
+        end
+
+        local mapping = tracedoc.changeset {
+            { "A" , map "A1" , "a" },
+            { "A" , map "A2" , "a" },
+            { "B" , map "B", "b" },
+            { "B" , map "BX" , "b.x" },
+            { add_b, "b.x", "b.y" },
+            { "C", add_c, "c" },
+        }
+
+        local function map(info)
+            print("====", info, "====")
+            tracedoc.mapchange(doc, mapping)
+            print("------------------")
+        end
+
+        map(1)
+
+        doc.a = 0
+
+        doc.b.y = 3
+        doc.c.b = 5
+
+        map(2)
+
+        doc.a = 1
+
+        doc.b.y = 4
+
+        map(3)
+
+        print("Filter A")
+        tracedoc.mapupdate(doc, mapping, "A")
+        print("Filter B")
+        tracedoc.mapupdate(doc, mapping, "B")
+        print("Filter null")
+        tracedoc.mapupdate(doc, mapping, "")
+        print("Filter All")
+        tracedoc.mapupdate(doc, mapping)
+    end)
+end)
---
+++ spec/legacy_test_spec.lua
@@ -0,0 +1,101 @@
+local tracedoc = require("tracedoc")
+
+describe("original test.lua from cloudwu/tracedoc", function()
+    local print = print
+    
+    local function dump(doc)
+        local changes = tracedoc.commit(doc, {})
+        print("Dump:")
+        for k,v in pairs(doc) do
+            print(k,v)
+            if type(v) == "table" then
+                for k, v in pairs(v) do
+                    print("\t",k,v)
+                end
+            end
+        end
+        print("changes:")
+        for k,v in pairs(changes) do
+            print(k,v)
+        end
+    end
+
+    setup(function()
+        print = function(...) end
+    end)
+
+    test("test.lua", function()
+        local doc = tracedoc.new {
+            a = 1,
+            b = { 1,2,3 },
+            c = { d = 4 , e = 5 },
+            d = {},
+        }
+        
+        dump(doc)
+        
+        print "-----------------------------"
+        
+        doc.b[3] = nil
+        doc.b = { 1,3 }	-- remove [3], change [2]
+        
+        dump(doc)
+        
+        tracedoc.opaque(doc.d, true)
+        doc.d.x = 1	-- d change ( d is opaque)
+        doc.d.y = 2	-- d change ( d is opaque)
+        
+        assert(doc.b[1] == 1)
+        assert(doc.b[2] == 3)
+        assert(doc.c.d == 4)
+        
+        doc.b[1] = 0	-- change
+        doc.b[2] = 2	-- not change
+        
+        local tmp = doc.c
+        
+        print(tracedoc.dump(doc.b))
+        
+        dump(doc)
+        
+        doc.a = nil	-- clear doc.a
+        assert(doc.a == nil)
+        
+        doc.b[3] = nil	-- remove 3
+        table.remove(doc.b)	-- remove 2
+        table.insert(doc.b , 5)	-- doc.b[2] = 5
+        table.insert(doc.b , 6)	-- doc.b[3] = 6
+        table.insert(doc.b , 7)	-- doc.b[4] = 7
+        
+        for k,v in ipairs(doc.b) do
+            print("doc.b[" .. k .. "]",v)
+        end
+        
+        doc.e = "e"	-- add doc.e
+        
+        for k,v in pairs(doc) do
+            print("doc."..k , v)
+        end
+        
+        dump(doc)
+        
+        doc.a = 2	-- change
+        assert(doc.a == 2)
+        doc.b = 2	-- change and delete table
+        assert(doc.b == 2)
+        local doc_c = { e = 5 }
+        doc.c = doc_c -- change table
+        assert(doc.c.d == nil)
+        doc.b = nil
+        doc.d = setmetatable({}, { __tostring = function() return "userobject" end })	-- table with metatable is an userobject
+        doc.e = { x = 1, y = 2 }
+        
+        assert(tmp == doc.c)
+        tmp = doc.c	-- update c
+        assert(tmp ~= doc_c)
+        
+        dump(doc)
+        
+        assert(tmp.e == 5)
+    end)
+end)
