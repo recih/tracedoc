5660a30a3dadad3e6f4bce5d66566769819b35fb
recih <fengbocih+github@gmail.com>
2018-06-01 14:54:56 +0800

    add more test code

---
+++ spec/basic_spec.lua
@@ -0,0 +1,176 @@
+local tracedoc = require("tracedoc")
+
+describe("basic tests", function()
+    local plain_data = {
+        hp = 50,
+        hp_max = 100,
+        level = 10,
+        skills = {
+            123,
+            456,
+            789,
+        },
+        pet = {
+            name = "dog",
+            level = 10,
+            hp = 100,
+        },
+    }
+    local doc = nil
+
+    before_each(function()
+        doc = tracedoc.new(plain_data) 
+    end)
+
+    after_each(function()
+        doc = nil
+    end)
+
+    it("should call commit after initialize to get initial state", function()
+        local changes = tracedoc.commit(doc, {})
+        assert.are_not.same(changes, {})
+    end)
+
+    it("should support pairs", function()
+        local function dump(doc)
+            local data = {}
+            for k, v in tracedoc.pairs(doc) do
+                if type(v) == "table" then
+                    data[k] = dump(v)
+                else
+                    data[k] = v
+                end
+            end
+            return data
+        end
+
+        -- before commit
+        local dump_data = dump(doc)
+        assert.are.same(dump_data, plain_data)
+
+        -- private data should not be accessed by pairs
+        assert.is_nil(dump_data._parent)
+        assert.is_nil(dump_data._dirty)
+        assert.is_nil(dump_data._changes)
+
+        -- after commit
+        tracedoc.commit(doc)
+        assert.are.same(dump(doc), plain_data)
+    end)
+
+    it("should support ipairs", function()
+        local function dump(doc)
+            local data = {}
+            for i, v in tracedoc.ipairs(doc) do
+                if type(v) == "table" then
+                    table.insert(data, dump(v))
+                else
+                    table.insert(data, v)
+                end
+            end
+            return data
+        end
+
+        -- before commit
+        local dump_data = dump(doc.skills)
+        assert.are.equal(tracedoc.len(doc.skills), #dump_data, #plain_data.skills)
+        assert.are.same(dump_data, plain_data.skills)
+
+        -- after commit
+        tracedoc.commit(doc)
+        assert.are.equal(tracedoc.len(doc.skills), #dump_data, #plain_data.skills)
+        assert.are.same(dump(doc.skills), plain_data.skills)
+    end)
+
+    test("field changes", function()
+        tracedoc.commit(doc)
+
+        doc.hp = doc.hp + 20
+        doc.level = 11
+
+        local changes = tracedoc.commit(doc, {})
+        assert.are.same(changes, {
+            _n = 2,
+            hp = 70,
+            level = 11,
+        })
+
+        -- test for no change
+        changes = tracedoc.commit(doc, {})
+        assert.are.same(changes, {})
+    end)
+
+    test("field add/remove", function()
+        tracedoc.commit(doc)
+        doc.mp = 90
+        doc.mp_max = 100
+        doc.buff = 123
+
+        local changes = tracedoc.commit(doc, {})
+        assert.are.same(changes, {
+            _n = 3,
+            mp = 90,
+            mp_max = 100,
+            buff = 123,
+        })
+
+        doc.buff = nil
+        changes = tracedoc.commit(doc, {})
+        assert.are.equal(changes.buff, tracedoc.null)
+    end)
+
+    test("no change", function()
+        tracedoc.commit(doc)
+
+        local changes = tracedoc.commit(doc, {})
+        assert.are.same(changes, {})
+    end)
+
+    test("table changes", function()
+        tracedoc.commit(doc)
+
+        doc.skills = {1, 2}
+        doc.pet = {
+            name = "cat",
+            level = 11,
+            hp = 100,
+        }
+        local changes = tracedoc.commit(doc, {})
+
+        assert.are.equal(changes["skills.1"], 1)
+        assert.are.equal(changes["skills.2"], 2)
+        assert.are.equal(changes["skills.3"], tracedoc.null)
+        assert.are.equal(changes["pet.name"], changes.pet.name, "cat")
+        assert.are.equal(changes["pet.level"], changes.pet.level, 11)
+        assert.is_nil(changes["pet.hp"])    -- pet.hp not changed
+        
+        assert.are.equal(tracedoc.len(changes.skills), 2)
+        for i, v in tracedoc.ipairs(changes.skills) do
+            assert.are.equal(changes["skills." .. i], v)
+        end
+
+        assert.are.equal(tracedoc.len(changes.pet), 0)
+    end)
+
+    test("opaque table", function()
+        tracedoc.opaque(doc.pet, true)
+        local changes = tracedoc.commit(doc, {})
+
+        -- pet is opaque, no details
+        assert.are.equal(changes["pet"], doc.pet)
+        assert.are.equal(changes["pet"].name, "dog")
+        assert.is_nil(changes["pet.hp"])
+        assert.is_nil(changes["pet.name"])
+        assert.is_nil(changes["pet.level"])
+
+        doc.pet = {
+            name = "cat",
+            level = 11,
+            hp = 100,
+        }
+        changes = tracedoc.commit(doc, {})
+
+        assert.are.equal(changes["pet"], doc.pet)
+        assert.are.equal(changes["pet"].name, "cat")
+    end)
+end)
---
+++ spec/changeset_spec.lua
@@ -0,0 +1,157 @@
+local tracedoc = require("tracedoc")
+
+describe("changeset tests", function()
+    local plain_data = {
+        level = 10,
+        name = "Player",
+        title = "",
+        hp = 100,
+        hp_max = 100,
+    }
+    local doc = nil
+
+    local print_buf = {}
+    local function _print(...)
+        -- print(...)
+        table.insert(print_buf, table.concat({...}, "\t"))
+    end
+
+    local function get_print_content()
+        return table.concat(print_buf, "\n")
+    end
+
+    local function clear_print_buf()
+        print_buf = {}
+    end
+
+    local function trim_lines(str)
+        return str:gsub("^%s+", ""):gsub("\n%s+", "\n")
+    end
+
+    local spies = {}
+    local function create_spy(name, func)
+        local s = spy.new(func)
+        spies[name] = s
+        return function(...)
+            return s(...)
+        end
+    end
+
+    local function clear_spies()
+        spies = {}
+    end
+
+    before_each(function()
+        doc = tracedoc.new(plain_data) 
+        clear_print_buf()
+        clear_spies()
+    end)
+
+    after_each(function()
+        doc = nil
+    end)
+
+    test("test mapchange/mapupdate", function()
+        local mapping = tracedoc.changeset {
+            {
+                "LEVEL",
+                create_spy("level", function(doc, level)
+                    _print("level = " .. level)
+                end),
+                "level",
+            },
+            {
+                -- no tag
+                create_spy("hp", function(doc, hp)
+                    _print("hp = " .. hp)
+                end),
+                "hp",
+            },
+            {
+                "HP_MAX",
+                create_spy("hp_max", function(doc, hp_max, buff)
+                    local hp_max_modify = buff and buff.hp_max_modify or 0
+                    hp_max_modify = hp_max_modify or 0
+                    _print("hp_max = " .. hp_max + hp_max_modify)
+                end),
+                "hp_max",
+                "buff",
+            },
+            {
+                "ITEM",
+                create_spy("item", function(doc, items)
+                    _print("items count = " .. tracedoc.len(items))
+                end),
+                "items",
+            },
+            {
+                "NAME",
+                create_spy("name", function(doc, name, title)
+                    local full_name = name
+                    if title and #title > 0 then
+                        full_name = full_name .. " - " .. title
+                    end
+                    _print("name = " .. full_name)
+                end),
+                "name",
+                "title",
+            },
+        }
+
+        tracedoc.mapchange(doc, mapping)
+        assert.spy(spies.level).was_called_with(doc, plain_data.level)
+        assert.spy(spies.hp).was_called_with(doc, plain_data.hp)
+        assert.spy(spies.name).was_called_with(doc, plain_data.name, plain_data.title)
+        assert.are.same(get_print_content(), trim_lines(
+            [[level = 10
+            hp = 100
+            hp_max = 100
+            name = Player]]))
+        clear_print_buf();
+
+        doc.title = "Super Man"
+        doc.hp = doc.hp + 10
+        tracedoc.mapchange(doc, mapping)
+        assert.spy(spies.hp).was_called_with(doc, plain_data.hp + 10)
+        assert.spy(spies.name).was_called_with(doc, plain_data.name, "Super Man")        
+        assert.are.same(get_print_content(), trim_lines(
+            [[hp = 110
+            name = Player - Super Man]]))
+        clear_print_buf();
+
+        doc.items = {1, 2, 3}
+        tracedoc.mapchange(doc, mapping)
+        assert.spy(spies.item).was_called()
+        assert.are.same(get_print_content(), trim_lines(
+            [[items count = 3]]))
+        clear_print_buf();
+
+        doc.buff = { hp_max_modify = 100}
+        tracedoc.mapchange(doc, mapping)
+        assert.spy(spies.hp_max).was_called()
+        assert.are.same(get_print_content(), trim_lines(
+            [[hp_max = 200]]))
+        clear_print_buf();
+
+        -- filter LEVEL tag
+        tracedoc.mapupdate(doc, mapping, "LEVEL")
+        assert.spy(spies.level).was_called()
+        assert.are.same(get_print_content(), trim_lines(
+            [[level = 10]]))
+        clear_print_buf();
+
+        -- filter no tag
+        tracedoc.mapupdate(doc, mapping, "")
+        assert.spy(spies.hp).was_called()
+        assert.are.same(get_print_content(), trim_lines(
+            [[hp = 110]]))
+        clear_print_buf();
+
+        -- filter all tag
+        tracedoc.mapupdate(doc, mapping)
+        assert.spy(spies.level).was_called()
+        assert.spy(spies.hp).was_called()
+        assert.spy(spies.item).was_called()
+        assert.spy(spies.name).was_called()
+    end)
+end)
